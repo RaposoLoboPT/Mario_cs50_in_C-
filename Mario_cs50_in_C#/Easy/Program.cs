c09558a5c42252953ef03e9b860ca6d4e1519288
Tiago Raposo Lobo <140200823+RaposoLoboPT@users.noreply.github.com>
2024-09-17 18:58:59 +0100

    Mario stairs level easy

---
+++ Mario_cs50_in_C#/Easy/Program.cs
@@ -0,0 +1,14 @@
+﻿int num;
+do
+{
+    Console.WriteLine("Diga um número entre 1 e 10:");
+    num = int.Parse(Console.ReadLine());
+} while (num < 1 || num > 10);
+
+string stairs = "#";
+
+for (int i = 0; i < num; i++)
+{
+    Console.WriteLine(stairs.PadLeft(num));
+    stairs += "#";
+}
